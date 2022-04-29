9e859c5e703652f111065f176a245ebaff20f3fc
Matthew Shiroma <19522250+maishiroma@users.noreply.github.com>
2022-04-28 22:28:30 -0700

    Added initial test game build

---
+++ Assets/Scripts/Builder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Builder : MonoBehaviour
+{
+   public static void Build()
+    {
+        BuildPlayerOptions options = new BuildPlayerOptions();
+        
+        options.options = BuildOptions.None;
+        options.scenes = new string[] { "Assets/Scenes/SampleScene.unity" };
+        options.target = BuildTarget.StandaloneWindows;
+        options.locationPathName = "Build";
+        options.targetGroup = BuildTargetGroup.Standalone;
+        
+        BuildPipeline.BuildPlayer(options);
+    }
+}
